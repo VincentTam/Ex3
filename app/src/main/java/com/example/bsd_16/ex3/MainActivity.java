133cf8f366b234045ca3ef604382c4f363b82546
Vincent Tam <sere@live.hk>
2015-08-17 11:46:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bsd_16/ex3/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.bsd_16.ex3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView3;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void clicked(View v) {
+        textView3 = (TextView) findViewById(R.id.textView3);
+        editText = (EditText) findViewById(R.id.editText);
+        String output_msg = "";
+
+        double score = Double.valueOf(editText.getText().toString());
+        if (score <= 100) {
+            char grade = grade(score);
+            output_msg += "Your grade is " + grade;
+        } else {
+            output_msg += "Max score is 100.";
+        }
+
+        textView3.setText(output_msg);
+    }
+
+    private char grade(double score) {
+        if (score >= 90)
+            return 'A';
+        if (score >= 80)
+            return 'B';
+        if (score >= 70)
+            return 'C';
+        if (score >= 60)
+            return 'D';
+        if (score >= 50)
+            return 'E';
+        else
+            return 'F';
+    }
+}
